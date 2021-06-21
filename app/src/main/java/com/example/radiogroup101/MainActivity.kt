282247f64be092735b1dfe64439f97962865423e
05443 <0544354125m@gmail.com>
2021-06-21 11:00:45 +0300

    2 buttons ,when you click ok , the text changes to the text of the selected button

--- app/src/main/java/com/example/radiogroup101/MainActivity.kt
+++ app/src/main/java/com/example/radiogroup101/MainActivity.kt
@@ -2,16 +2,36 @@ package com.example.radiogroup101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
 
     private val radioGroup:RadioGroup by lazy { findViewById(R.id.rG) }
+    private val btnOk:Button by lazy { findViewById(R.id.buttonOk) }
+    private val btnCancel:Button by lazy { findViewById(R.id.buttonCancel) }
+    private val tvTitle:TextView by lazy { findViewById(R.id.tvTitle) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnOk.setOnClickListener {
+
+            val selectionId = radioGroup.checkedRadioButtonId
+            val selectionButton:Button =findViewById(selectionId)
+            tvTitle.text = selectionButton.text
+        }
+        btnCancel.setOnClickListener {
+            radioGroup.clearCheck()
+            tvTitle.text = "hello world!!!"
+        }
+        tvTitle.setOnClickListener {
+            radioGroup.clearCheck()
+        }
 
 
     }
