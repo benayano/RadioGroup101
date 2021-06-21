76dfe317e133e3d7a9a6d2795331d96d4c80210d
05443 <0544354125m@gmail.com>
2021-06-21 11:27:20 +0300

    Correction , when no button is selected an appropriate message will be displayed

--- app/src/main/java/com/example/radiogroup101/MainActivity.kt
+++ app/src/main/java/com/example/radiogroup101/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.example.radiogroup101
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
 
-    private val radioGroup:RadioGroup by lazy { findViewById(R.id.rG) }
-    private val btnOk:Button by lazy { findViewById(R.id.buttonOk) }
-    private val btnCancel:Button by lazy { findViewById(R.id.buttonCancel) }
-    private val tvTitle:TextView by lazy { findViewById(R.id.tvTitle) }
+    private val radioGroup: RadioGroup by lazy { findViewById(R.id.rG) }
+    private val btnOk: Button by lazy { findViewById(R.id.buttonOk) }
+    private val btnCancel: Button by lazy { findViewById(R.id.buttonCancel) }
+    private val tvTitle: TextView by lazy { findViewById(R.id.tvTitle) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
         btnOk.setOnClickListener {
 
             val selectionId = radioGroup.checkedRadioButtonId
-            val selectionButton:Button =findViewById(selectionId)
-            tvTitle.text = selectionButton.text
+            if (selectionId != -1) {
+                val selectionButton: Button = findViewById(selectionId)
+
+                tvTitle.text = selectionButton.text
+
+            }else{
+                Snackbar.make(tvTitle,"the result is $selectionId",Snackbar.LENGTH_LONG).show()
+            }
         }
         btnCancel.setOnClickListener {
             radioGroup.clearCheck()
